45e5d856aad80f9e246c51b09c51ab327a5884f6
mohanreddy819 <simplemail819@gmail.com>
2025-08-18 01:50:23 +0530

    temp-fix7- fix

--- handlers/health.go
+++ handlers/health.go
@@ -1,10 +1,7 @@
 // health.go
 package handlers
 
-import (
-	"encoding/json"
-	"net/http"
-)
+
 
 // just to write a basic test case observe on error and pass of the pipeline
 func AddNumber(a,b int) int{
