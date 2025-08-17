5793ce726f3d6344debbd237414f3286e1d1c527
mohanreddy819 <simplemail819@gmail.com>
2025-08-17 20:18:02 +0530

    temp-fix1

---
+++ handlers/health.go
@@ -0,0 +1,12 @@
+// health.go
+package todo
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// just to write a basic test case observe on error and pass of the pipeline
+func AddNumber(a,b int) int{
+	return a+b
+}
---
+++ test/health_test.go
@@ -0,0 +1,12 @@
+package test
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "handlers"
+)
+
+func TestTodoHandler(t *Testing.T){
+	req:= AddNumber(2,2)
+
+}
